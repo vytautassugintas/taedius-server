8959952756464932478fa2015b9f2b7933eeebbf
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2018-02-27 00:28:51 +0200

    chore: tslint

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get('/', (req: any, res: any) => res.send('Hello World!'))
+app.get('/', (req: any, res: any) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
 export default app;
