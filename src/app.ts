209164007dfd9fae691ee1497dc7a13a6ed8a980
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2018-02-27 21:57:12 +0200

    chore: run with watch

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req: any, res: any) => res.send('Hello World!'));
+app.get('/', (req: any, res: any) => res.send('Hello world!'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
