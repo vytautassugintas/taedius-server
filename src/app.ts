5bf08ddac0473bf8ea368115673600d6920dd6f4
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2018-02-28 00:00:41 +0200

    chore: testing mongo connection

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,25 @@
 import express from 'express';
-
+import  mongoose from 'mongoose';
 const app = express();
+
+mongoose.connect('mongodb://localhost/test');
+
+const db = mongoose.connection;
+
+const userSchema = new mongoose.Schema({
+    name: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('connected');
+  User.find((err, users) => {
+      if (err) console.log(err);
+      console.log(users);
+  });
+});
 
 app.get('/', (req: any, res: any) => res.send('Hello world!'));
 
