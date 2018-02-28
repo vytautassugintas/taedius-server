21d5c5779881c871acaf16837972ec881923ddae
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2018-03-01 00:51:06 +0200

    feat: sign up

---
+++ src/controllers/auth.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+import { default as User, UserModel } from '../models/User';
+
+const request = require('express-validator');
+
+export let signup = (req: Request, res: Response, next: NextFunction) => {
+    // TODO: validate request
+
+    const user = new User({
+      email: req.body.email,
+      password: req.body.password
+    });
+
+    User.findOne({ email: req.body.email }, (err, existingUser) => {
+      if (err) { return next(err); }
+      if (existingUser) {
+        return res.json({message: 'user exists'});
+      }
+
+      user.save((err) => {
+        if (err) { return next(err); }
+        req.logIn(user, (err) => {
+          if (err) {
+            return next(err);
+          }
+          return res.json({message: 'success'});
+        });
+      });
+    });
+  };
